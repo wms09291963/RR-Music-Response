a1afa0e75b9161a91d8bf5809f777ab967e0f254
Smith <william.smith@bankerslife.com>
2022-08-26 14:27:56 -0400

    exercise 5

---
+++ music_library/src/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import { searchContext } from '../context/SearchContext'
+
+function SearchBar() {
+    const {term, handleSearch} = useContext(SearchContext)
+
+    return (
+        <form>
+            <input ref={term} type="text" placeholder="Search Here" />
+            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+        </form
+    >)
+}
+
+
+export default SearchBar
---
+++ music_library/src/context/SearchContext.js
@@ -0,0 +1,10 @@
+import {createContext} from 'react'
+
+
+
+
+
+export const SearchContext = createContext({
+    term: '',
+    handleSearch: () => {}
+})
